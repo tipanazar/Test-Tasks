4799f2f2ce98a78a01adc4b81d6ada595d09d14f
Nazar <karpeko76@gmail.com>
2022-09-18 15:11:35 +0100

    upd

--- JS 13.09.22/index.js
+++ JS 13.09.22/index.js
@@ -1,28 +1,30 @@
-let biggestNum = 0;
-let totalGrain = 0;
+let leftIdx = 0;
+let left = 0;
+let right = 0;
+let result = 0;
 
 function loadGrain(numArr) {
-  for (let num of numArr) {
-    biggestNum < num
-      ? (biggestNum = num)
-      : biggestNum > numArr[numArr.length - 1]
-      ? (totalGrain = totalGrain + numArr[numArr.length - 1] - num)
-      : (totalGrain = totalGrain + biggestNum - num);
-  }
+  let rightIdx = numArr.length - 1;
 
-  if (totalGrain < 0) {
-    totalGrain = 0;
-  }
+  while (leftIdx < rightIdx) {
+    left = numArr[leftIdx] > left ? numArr[leftIdx] : left;
 
-  // console.log(totalGrain);
-  return totalGrain;
+    right = numArr[rightIdx] > right ? numArr[rightIdx] : right;
+
+    left >= right
+      ? ((result += right - numArr[rightIdx]), rightIdx--)
+      : ((result += left - numArr[leftIdx]), leftIdx++);
+  }
+  console.log(result);
+  return result;
 }
 
+// loadGrain([30, 44, 6, 17, 49, 30, 20, 26, 47, 44, 28, 6, 9, 9, 25, 23, 22, 10]); // 181
 // loadGrain([2, 1, 5, 2, 7, 4, 10]); // 7
 // loadGrain([2, 0, 1, 5, 2, 7]); // 6
 // loadGrain([4, 0, 1, 3]); // 5
 // loadGrain([4, 1, 3]); // 2
 // loadGrain([5, 5, 2]); // 0
 // loadGrain([2, 4, 2]); // 0
-// loadGrain([7, 4]); // 0
+// loadGrain([90, 80]); // 0
 // loadGrain([]); // 0
